e7651a88422f427310e88d683a84faaf91ce57dd
Emery Vickers <emrvick@iu.edu>
2023-10-30 19:26:49 -0400

    Touchable Opacities added

--- EZRecipes/components/navCard.js
+++ EZRecipes/components/navCard.js
@@ -1,24 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function NavCard({imgURI,text}){
     const navigation = useNavigation();
     return(
-        <ImageBackground style={styles.navButtonImg} source={imgURI}>
-            <Text style={styles.navButtonText} onPress={() => navigation.navigate("Categories")}>{text}</Text>
-        </ImageBackground>
+        <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("Categories")}>
+            <ImageBackground style={styles.navButtonImg} source={imgURI}>
+                <Text style={styles.navButtonText}>{text}</Text>
+            </ImageBackground>
+        </TouchableOpacity>
     )
 }
 const styles = StyleSheet.create({
-    navButtonImg: {
+    navButton: {
         width: '100%',
         flex: 1,
+        borderColor: '#351100',
+        borderBottomWidth: 4,
+      },
+      navButtonImg: {
+        width: '100%',
+        height: '100%',
         justifyContent: 'flex-end',
         alignItems: 'center',
         textAlign: 'center',
-        borderColor: '#351100',
-        borderBottomWidth: 4,
       },
       navButtonText: {
         backgroundColor: '#351100',
